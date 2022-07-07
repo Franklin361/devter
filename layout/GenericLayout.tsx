22e69f5854576784bb31259b9be1a39f58ec54f9
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-07 13:54:21 -0500

    [FEAT] crete generic layout to Home page

---
+++ layout/GenericLayout.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+interface Props {
+  children: JSX.Element | JSX.Element[]
+  title?: string;
+}
+
+export const GenericLayout = ({ children, title }: Props) => {
+  return (
+    <>
+        {
+          title && <Head><title>{ title }</title></Head>
+        }
+        
+        <div className='absolute top-0 left-0 triangle bg-neutral w-screen h-screen -z-10' />
+        {children}
+    </>
+  )
+}
