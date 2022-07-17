d71aa9f038c9888b345736ed30cb20718eaa1326
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-17 10:52:15 -0500

    [STYLE] add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,16 +9,62 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Devter</title>
-        <meta
-          name="description"
-          content="App for developers to create some tweets"
-        />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <link
           rel="shortcut icon"
-          href="https://res.cloudinary.com/dnxchppfm/image/upload/v1646238992/rocket_tpjxym.webp"
+          href="https://res.cloudinary.com/dnxchppfm/image/upload/c_scale,w_904/v1658070841/perspective_matte_mtiwmt.webp"
           type="image/x-icon"
         />
+
+        <meta
+          name="description"
+          content="Devter | Share your dev posts with the community 🐦."
+        />
+        <meta name="author" content="Franklin Martinez" />
+        <meta
+          name="title"
+          content="Devter is a web application where you can create publications about web development and share them with the tech community!"
+        />
+        <meta
+          name="image"
+          content="https://res.cloudinary.com/dnxchppfm/image/upload/v1658071018/devter/mian-devter_jfor64.webp"
+        />
+
+        <meta
+          property="og:title"
+          content="Devter is a web application where you can create publications about web development and share them with the tech community!"
+        />
+        <meta
+          property="og:description"
+          content="Devter | Share your dev posts with the community 🐦."
+        />
+        <meta property="og:url" content="https://devter.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dnxchppfm/image/upload/v1658071018/devter/mian-devter_jfor64.webp"
+        />
+        <meta property="og:type" content="website" />
+
+        <meta
+          name="twitter:title"
+          content="Devter is a web application where you can create publications about web development and share them with the tech community!"
+        />
+        <meta
+          name="twitter:description"
+          content="Devter | Share your dev posts with the community 🐦."
+        />
+        <meta name="twitter:url" content="https://devter.vercel.app" />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dnxchppfm/image/upload/v1658071018/devter/mian-devter_jfor64.webp"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Frankomtz361" />
+        <meta name="twitter:creator" content="@Frankomtz361" />
+
+        <title>Devter</title>
       </Head>
       <AuthProvider>
         <Component {...pageProps} />
